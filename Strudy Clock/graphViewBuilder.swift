a8498b40fea98ccaaf4b3f8368e61d6d184ad1cf
DAZABAMUKER <wookis112@gmail.com>
2023-10-30 20:25:34 +0900

    통계뷰 초안 완성, 다크 모드 지원

---
+++ Strudy Clock/graphViewBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  graphViewBuilder.swift
+//  Strudy Clock
+//
+//  Created by 안병욱 on 10/30/23.
+//
+
+import SwiftUI
+
+struct graphViewBuilder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    graphViewBuilder()
+}
