0e60eddf56d312e9e46437c8a66f330ce8a78f01
DAZABAMUKER <wookis112@gmail.com>
2023-11-07 16:42:55 +0900

    그래프 디테일 추가

---
+++ Strudy Clock/GraphDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  GraphDetailView.swift
+//  Strudy Clock
+//
+//  Created by 안병욱 on 11/7/23.
+//
+
+import SwiftUI
+
+struct GraphDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GraphDetailView()
+}
