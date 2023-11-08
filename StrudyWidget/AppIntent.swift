290ed55e0010214df47bf8a153eceec61e4f963d
DAZABAMUKER <wookis112@gmail.com>
2023-11-08 09:18:52 +0900

    등듣

---
+++ StrudyWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  StrudyWidget
+//
+//  Created by 안병욱 on 11/7/23.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ StrudyWidget/StrudyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StrudyWidgetBundle.swift
+//  StrudyWidget
+//
+//  Created by 안병욱 on 11/7/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StrudyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StrudyWidget()
+    }
+}
